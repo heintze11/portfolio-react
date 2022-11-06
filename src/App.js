07d7feff35d15088e18335f6a5fe03baa8a743b7
Levi H <levi.heintzelman@gmail.com>
2022-11-06 15:11:42 -0700

    All components added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
 import './App.css';
 
 const App = () => {
@@ -8,6 +13,11 @@ const App = () => {
     <>
       <Navbar />
       <Header />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Resume />
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="text-center m-1 p-1">
+        <a className='footer-item m-1 p-1' href="https://github.com/heintze11">Github</a>
+        <a className='footer-item m-1 p-1' href="https://www.linkedin.com/in/leviheintzelman/">LinkedIn</a>
+        <a className='footer-item m-1 p-1' href="https://stackoverflow.com/users/19106679/levi-h">Stack Overflow</a>
+    </footer>
+  )
+}
+
+export default Footer
